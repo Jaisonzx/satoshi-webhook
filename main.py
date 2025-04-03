8433fd75cdc78f9c968f3cc8b664c18e1dcc797e
Jaisonzx <105252102+Jaisonzx@users.noreply.github.com>
2025-04-03 00:03:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from pyrogram.types import ChatInviteLink
 import requests
 from datetime import datetime, timedelta
 import threading
+import asyncio
 
-# CONFIG
+# CONFIGS
 api_id = 21884784
 api_hash = "4bd2d2de4ed0d1662bbe341e95280e95"
 session_name = "satoshi"
@@ -13,11 +14,16 @@ chat_id = -1002531479445
 nome_do_link = "Aprovação ADM"
 webhook_destino = "https://webhook.flowzin.site/webhook/satoshi-aprovação"
 
-# INICIA CLIENTE
-app_pyro = Client(session_name, api_id=api_id, api_hash=api_hash)
-
 # FLASK
 app = Flask(__name__)
+
+# CLIENTE PYROGRAM GLOBAL
+app_pyro = Client(session_name, api_id=api_id, api_hash=api_hash)
+pyro_loop = asyncio.new_event_loop()
+
+def start_pyrogram():
+    asyncio.set_event_loop(pyro_loop)
+    app_pyro.run()
 
 @app.route("/gera-link", methods=["POST"])
 def gerar_link():
@@ -26,12 +32,17 @@ def gerar_link():
 
         expire_date = datetime.utcnow() + timedelta(days=1)
 
-        link: ChatInviteLink = app_pyro.create_chat_invite_link(
-            chat_id=chat_id,
-            expire_date=expire_date,
-            member_limit=1,
-            name=nome_do_link
+        future = asyncio.run_coroutine_threadsafe(
+            app_pyro.create_chat_invite_link(
+                chat_id=chat_id,
+                expire_date=expire_date,
+                member_limit=1,
+                name=nome_do_link
+            ),
+            pyro_loop
         )
+
+        link: ChatInviteLink = future.result()
 
         print(f"✅ Link gerado: {link.invite_link}")
 
@@ -54,5 +65,5 @@ def start_flask():
     app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
-    app_pyro.start()
+    threading.Thread(target=start_pyrogram).start()
     threading.Thread(target=start_flask).start()
