041e88494188b30507ca242800e02ebc764b73d7
Jaisonzx <105252102+Jaisonzx@users.noreply.github.com>
2025-04-03 00:01:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from pyrogram import Client
 from pyrogram.types import ChatInviteLink
 import requests
 from datetime import datetime, timedelta
+import threading
 
-# TELEGRAM CONFIG
+# CONFIG
 api_id = 21884784
 api_hash = "4bd2d2de4ed0d1662bbe341e95280e95"
 session_name = "satoshi"
@@ -12,7 +13,7 @@ chat_id = -1002531479445
 nome_do_link = "Aprovação ADM"
 webhook_destino = "https://webhook.flowzin.site/webhook/satoshi-aprovação"
 
-# PYROGRAM
+# INICIA CLIENTE
 app_pyro = Client(session_name, api_id=api_id, api_hash=api_hash)
 
 # FLASK
@@ -23,8 +24,6 @@ def gerar_link():
     try:
         print("🚀 Criando link de convite...")
 
-        app_pyro.start()
-
         expire_date = datetime.utcnow() + timedelta(days=1)
 
         link: ChatInviteLink = app_pyro.create_chat_invite_link(
@@ -33,8 +32,6 @@ def gerar_link():
             member_limit=1,
             name=nome_do_link
         )
-
-        app_pyro.stop()
 
         print(f"✅ Link gerado: {link.invite_link}")
 
@@ -52,6 +49,10 @@ def gerar_link():
         print("❌ ERRO:", str(e))
         return jsonify({"erro": str(e)}), 500
 
-if __name__ == "__main__":
-    print("🟢 Servidor iniciado em http://0.0.0.0:5000")
+def start_flask():
+    print("🟢 Servidor Flask em execução em http://0.0.0.0:5000")
     app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    app_pyro.start()
+    threading.Thread(target=start_flask).start()
